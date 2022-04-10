de9cae5891ad09f14fcdbaa0e6a3ed79ca6e36a8
Evance23 <evancebarracks01@gmail.com>
2022-04-10 11:17:42 +0300

    Add all changes to proj

--- Desktop/to-do/todo/src/app/app.module.ts
+++ Desktop/to-do/todo/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { TaskComponent } from './task/task.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    TaskComponent
+    TaskComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
