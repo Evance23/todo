3d437d0dcda84ae7974605634f00d20c026d51b8
Evance23 <evancebarracks01@gmail.com>
2022-04-11 21:59:44 +0300

    Add angular material to imports

--- Desktop/to-do/todo/src/app/app.module.ts
+++ Desktop/to-do/todo/src/app/app.module.ts
@@ -2,20 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+import { AngularMaterialModule } from './angular-material.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { TaskComponent } from './task/task.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     TaskComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
+    SigninComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    AngularMaterialModule,
     AppRoutingModule,
     FormsModule
   ],
